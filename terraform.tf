ac3aa45d8b1f54ec30434a4f24148fb29fe55005
Robin Venables <robin@robinvenables.com>
2021-05-11 15:16:44 +0100

    Added config for Villa de Tara

--- terraform.tf
+++ terraform.tf
@@ -4,6 +4,6 @@ terraform {
     bucket = "venables-terraform"
     dynamodb_table = "terraform-state-lock"
     region = "eu-west-2"
-    key = "aws-s3-hosted-website/terraform.tfstate"
+    # key = "aws-s3-hosted-website/terraform.tfstate"
   }
 }
---
+++ villa.tfvars
@@ -0,0 +1,2 @@
+root_domain_name = "villadetara.com"
+region           = "eu-west-2"
